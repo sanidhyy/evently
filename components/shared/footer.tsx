91aa23852407d15e6a9719ef236c81e5c7d21eee
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-17 06:43:36 +0000

    footer completed

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -1,3 +1,25 @@
+import { Copyright } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="Evently logo"
+            width={128}
+            height={38}
+          />
+        </Link>
+
+        <p className="flex items-center justify-center gap-x-1">
+          <Copyright className="h-4 w-4" /> {new Date().getFullYear()} Evently.
+          All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
 };
