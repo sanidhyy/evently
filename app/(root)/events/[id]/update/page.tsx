13e5dc76d3939787e8e8c3ae49996d98428fe94c
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-17 15:46:39 +0000

    create and update events structure created

---
+++ app/(root)/events/[id]/update/page.tsx
@@ -0,0 +1,25 @@
+import { auth } from "@clerk/nextjs";
+
+import { EventForm } from "@/components/shared/event-form";
+
+const UpdateEvent = () => {
+  const { sessionClaims } = auth();
+
+  const userId = sessionClaims?.userId as string;
+
+  return (
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <h3 className="wrapper h3-bold text-center sm:text-left">
+          Update Event
+        </h3>
+      </section>
+
+      <div className="wrapper my-8">
+        <EventForm userId={userId} type="Update" />
+      </div>
+    </>
+  );
+};
+
+export default UpdateEvent;
