747953caf1605a5eff6c36c06885b5b40fe8727b
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-18 11:04:35 +0000

    uploadthing callback fixes

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -4,4 +4,9 @@ import { ourFileRouter } from "./core";
 
 export const { GET, POST } = createNextRouteHandler({
   router: ourFileRouter,
+  config: {
+    uploadthingId: process.env.UPLOADTHING_APP_ID,
+    uploadthingSecret: process.env.UPLOADTHING_SECRET,
+    callbackUrl: `${process.env.NEXT_PUBLIC_APP_BASE_URL}/api/uploadthing`,
+  },
 });
