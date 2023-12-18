94cd9fd5871e72e55dca9101cf7daa305687fef8
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-18 11:31:28 +0000

    event details page ui implemented

---
+++ app/(root)/events/[id]/page.tsx
@@ -0,0 +1,91 @@
+import { getEventById } from "@/lib/actions/event.actions";
+import { formatDateTime } from "@/lib/utils";
+import type { SearchParamProps } from "@/types";
+import Image from "next/image";
+
+const EventDetails = async ({ params: { id } }: SearchParamProps) => {
+  const event = await getEventById(id);
+
+  return (
+    <section className="flex justify-center bg-primary-50 bg-dotted-pattern bg-contain">
+      <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl">
+        <Image
+          src={event.imageUrl}
+          alt="hero image"
+          width={1000}
+          height={1000}
+          className="h-full min-h-[300px] object-cover object-center"
+        />
+
+        <div className="flex w-full flex-col gap-8 p-5 md:p-10">
+          <div className="flex flex-col gap-6">
+            <h2 className="h2-bold">{event.title}</h2>
+
+            <div className="flex flex-col gap-3 sm:flex-row sm:items-center">
+              <div className="flex gap-3">
+                <p className="p-bold-20 rounded-full bg-green-500/10 px-5 py-2 text-green-700">
+                  {event.isFree ? "FREE" : `$${event.price}`}
+                </p>
+
+                <p className="p-medium-16 rounded-full bg-grey-500/10 px-4 py-2.5 text-grey-500">
+                  {event.category.name}
+                </p>
+              </div>
+
+              <p className="p-medium-18 ml-2 mt-2 sm:mt-0">
+                by{" "}
+                <span className="text-primary-500">
+                  {event.organizer.firstName} {event.organizer.lastName}
+                </span>
+              </p>
+            </div>
+          </div>
+
+          {/* TODO: CHECKOUT BUTTON */}
+
+          <div className="flex flex-col gap-5">
+            <div className="flex gap-2 md:gap-3">
+              <Image
+                src="/assets/icons/calendar.svg"
+                alt="Calendar"
+                width={32}
+                height={32}
+              />
+
+              <div className="p-medium-16 lg:p-regular-20 flex flex-wrap items-center">
+                <p>
+                  {formatDateTime(event.startDateTime).dateOnly} -{" "}
+                  {formatDateTime(event.startDateTime).timeOnly}
+                </p>
+                <p>
+                  {formatDateTime(event.endDateTime).dateOnly} -{" "}
+                  {formatDateTime(event.endDateTime).timeOnly}
+                </p>
+              </div>
+            </div>
+
+            <div className="p-regular-20 flex items-center gap-3">
+              <Image
+                src="/assets/icons/location.svg"
+                alt="location"
+                width={32}
+                height={32}
+              />
+              <p className="p-medium-16 lg:p-regular-20">{event.location}</p>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <p className="p-bold-20 text-gray-600">What you'll Learn:</p>
+            <p className="p-medium-16 lg:p-regular-18">{event.description}</p>
+            <p className="p-medium-16 lg:p-regular-18 truncate text-primary-500 underline">
+              {event.url}
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default EventDetails;
