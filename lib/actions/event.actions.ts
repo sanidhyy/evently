2815a814b453552578a68a479eedcbc295d77881
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-23 11:12:37 +0000

    fetch all event action implemented

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -5,7 +5,7 @@ import Category from "@/lib/database/models/category.model";
 import Event from "@/lib/database/models/event.model";
 import User from "@/lib/database/models/user.model";
 import { handleError } from "@/lib/utils";
-import type { CreateEventParams } from "@/types";
+import type { CreateEventParams, GetAllEventsParams } from "@/types";
 
 const populateEvent = async (query: any) => {
   return query
@@ -54,6 +54,34 @@ export const getEventById = async (eventId: string) => {
     if (!event) throw new Error("Event not found.");
 
     return JSON.parse(JSON.stringify(event));
+  } catch (error: unknown) {
+    handleError(error);
+  }
+};
+
+export const getAllEvents = async ({
+  query,
+  limit = 6,
+  page,
+  category,
+}: GetAllEventsParams) => {
+  try {
+    await connectToDatabase();
+
+    const conditions = {};
+
+    const eventsQuery = Event.find(conditions)
+      .sort({ createdAt: "desc" })
+      .skip(0)
+      .limit(limit);
+
+    const events = await populateEvent(eventsQuery);
+    const eventsCount = await Event.countDocuments(conditions);
+
+    return {
+      data: JSON.parse(JSON.stringify(events)),
+      totalPages: Math.ceil(eventsCount / limit),
+    };
   } catch (error: unknown) {
     handleError(error);
   }
