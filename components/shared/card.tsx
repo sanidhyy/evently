573e1ad84a0b6a38be2a049543af819481ef4992
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-23 11:35:06 +0000

    collection card implemented

---
+++ components/shared/card.tsx
@@ -0,0 +1,85 @@
+import { auth } from "@clerk/nextjs";
+import Link from "next/link";
+import Image from "next/image";
+
+import { IEvent } from "@/lib/database/models/event.model";
+import { formatDateTime } from "@/lib/utils";
+
+type CardProps = {
+  event: IEvent;
+  hasOrderLink?: boolean;
+  hidePrice?: boolean;
+};
+
+export const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
+  const { sessionClaims } = auth();
+  const userId = sessionClaims?.userId as string;
+
+  const isEventCreator = userId === event.organizer._id.toString();
+
+  return (
+    <div className="group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition-all hover:shadow-lg md:min-h-[438px]">
+      <Link
+        href={`/events/${event._id}`}
+        style={{
+          backgroundImage: `url(${event.imageUrl})`,
+        }}
+        className="flex-center flex-grow bg-gray-50 bg-cover bg-center text-gray-500"
+      />
+
+      {isEventCreator && !hidePrice && (
+        <div className="absolute right-2 top-2 flex flex-col gap-4 rounded-xl bg-white p-3 shadow-sm transition-all">
+          <Link href={`/events/${event._id}/update`}>
+            <Image
+              src="/assets/icons/edit.svg"
+              alt="edit"
+              width={20}
+              height={20}
+            />
+          </Link>
+        </div>
+      )}
+
+      <Link
+        href={`/events/${event._id}`}
+        className="flex min-h-[238px] flex-col gap-3 p-5 md:gap-4"
+      >
+        {!hidePrice && (
+          <div className="flex gap-2">
+            <span className="p-semibold-14 w-min rounded-full bg-green-100 px-4 py-1 text-green-600">
+              {event.isFree ? "FREE" : `$${event.price}`}
+            </span>
+            <p className="p-semibold-14 w-min rounded-full bg-gray-500/10 px-4 py-1 text-gray-500">
+              {event.category.name}
+            </p>
+          </div>
+        )}
+
+        <p className="p-medium-16 p-medium-18 text-gray-500">
+          {formatDateTime(event.startDateTime).dateTime}
+        </p>
+        <p className="p-medium016 md:p-medium-20 line-clamp-2 flex-1 text-black">
+          {event.title}
+        </p>
+
+        <div className="flex-between w-full">
+          <p className="p-medium-14 md:p-medium-16 text-gray-600">
+            {event.organizer.firstName} {event.organizer.lastName}
+          </p>
+
+          {hasOrderLink && (
+            <Link href={`/orders?eventId=${event._id}`} className="flex gap-2">
+              <p className="text-primary-500">Order Details</p>
+              <Image
+                src="/assets/icons/arrow.svg"
+                alt="search"
+                width={10}
+                height={10}
+              />
+            </Link>
+          )}
+        </div>
+      </Link>
+    </div>
+  );
+};
--- components/shared/collection.tsx
+++ components/shared/collection.tsx
@@ -1,5 +1,7 @@
 import { IEvent } from "@/lib/database/models/event.model";
 
+import { Card } from "./card";
+
 type CollectionProps = {
   data: IEvent[];
   emptyTitle: string;
@@ -20,5 +22,33 @@ export const Collection = ({
   collectionType,
   urlParamName,
 }: CollectionProps) => {
-  return <div className="">Collection</div>;
+  return (
+    <>
+      {data.length > 0 ? (
+        <div className="flex flex-col items-center gap-10">
+          <ul className="grid w-full grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:gap-10">
+            {data.map((event) => {
+              const hasOrderLink = collectionType === "Events_Organized";
+              const hidePrice = collectionType === "My_Tickets";
+
+              return (
+                <li className="flex justify-center" key={event._id}>
+                  <Card
+                    event={event}
+                    hasOrderLink={hasOrderLink}
+                    hidePrice={hidePrice}
+                  />
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      ) : (
+        <div className="flex-center wrapper min-h-[200px] w-full flex-col gap-3 rounded-[14px] bg-gray-50 py-28 text-center">
+          <h3 className="p-bold-20 md:h5-bold">{emptyTitle}</h3>
+          <p className="p-regular-14">{emptyStateSubtext}</p>
+        </div>
+      )}
+    </>
+  );
 };
