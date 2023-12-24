3015b1705932b97f3ad980f70c7eb786e79e697d
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-24 07:38:26 +0000

    checkout button implemented

---
+++ components/shared/checkout-button.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { SignedIn, SignedOut, useUser } from "@clerk/nextjs";
+import Link from "next/link";
+
+import { buttonVariants } from "@/components/ui/button";
+import type { IEvent } from "@/lib/database/models/event.model";
+import { cn } from "@/lib/utils";
+
+import { Checkout } from "./checkout";
+
+export const CheckoutButton = ({ event }: { event: IEvent }) => {
+  const { user } = useUser();
+  const userId = user?.publicMetadata.userId as string;
+  const hasEventFinished = new Date(event.endDateTime) < new Date();
+
+  return (
+    <div className="flex items-center gap-3">
+      {/* Cannot buy past event */}
+      {hasEventFinished ? (
+        <p className="p-2 text-red-400">
+          Sorry, tickets are no longer available.
+        </p>
+      ) : (
+        <>
+          <SignedOut>
+            <Link
+              href="/sign-in"
+              className={cn(
+                buttonVariants({
+                  size: "lg",
+                }),
+                "button rounded-full"
+              )}
+            >
+              Get Tickets
+            </Link>
+          </SignedOut>
+
+          <SignedIn>
+            <Checkout event={event} userId={userId} />
+          </SignedIn>
+        </>
+      )}
+    </div>
+  );
+};
